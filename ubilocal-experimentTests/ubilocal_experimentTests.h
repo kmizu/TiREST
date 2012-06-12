af4ddf58a42082d84a13fdaa1efa160c1e939a9e
Kota Mizushima <mizukota@gmail.com>
2012-06-12 14:08:24 +0900

    Internal commit.

---
+++ ubilocal-experimentTests/ubilocal_experimentTests.h
@@ -0,0 +1,13 @@
+//
+//  ubilocal_experimentTests.h
+//  ubilocal-experimentTests
+//
+//  Created by Mizushima Kota on 12/06/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ubilocal_experimentTests : SenTestCase
+
+@end
---
+++ ubilocal-experimentTests/ubilocal_experimentTests.m
@@ -0,0 +1,32 @@
+//
+//  ubilocal_experimentTests.m
+//  ubilocal-experimentTests
+//
+//  Created by Mizushima Kota on 12/06/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ubilocal_experimentTests.h"
+
+@implementation ubilocal_experimentTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ubilocal-experimentTests");
+}
+
+@end
