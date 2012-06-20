4766528faf32348dc931d2996f3775839bc1de31
Kota Mizushima <mizukota@gmail.com>
2012-06-20 14:35:36 +0900

    Refactoring: extract common TiRESTFunctionnalTestBase class

---
+++ Tests/TiRESTSimpleTestCase.h
@@ -0,0 +1,13 @@
+//
+//  TiRESTSimpleTestCase.h
+//  TiREST
+//
+//  Created by Kota Mizushima on 12/06/20.
+//
+//
+
+#import "TiRESTFunctionalTests.h"
+
+@interface TiRESTSimpleTestCase : TiRESTFunctionalTests
+
+@end
---
+++ Tests/TiRESTSimpleTestCase.m
@@ -0,0 +1,13 @@
+//
+//  TiRESTSimpleTestCase.m
+//  TiREST
+//
+//  Created by Kota Mizushima on 12/06/20.
+//
+//
+
+#import "TiRESTSimpleTestCase.h"
+
+@implementation TiRESTSimpleTestCase
+
+@end
