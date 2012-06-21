801edac8a15bfaaadec5dac750e3f05b0deec5b3
Kota Mizushima <mizukota@gmail.com>
2012-06-21 16:35:28 +0900

    Add TRRoutingTable, will be separated from TRRequestRouter

---
+++ Classes/Routing/TRRoutingTable.h
@@ -0,0 +1,13 @@
+//
+//  TRRoutingTable.h
+//  TiREST
+//
+//  Created by Kota Mizushima on 12/06/21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TRRoutingTable : NSObject
+
+@end
---
+++ Classes/Routing/TRRoutingTable.m
@@ -0,0 +1,13 @@
+//
+//  TRRoutingTable.m
+//  TiREST
+//
+//  Created by Kota Mizushima on 12/06/21.
+//
+//
+
+#import "TRRoutingTable.h"
+
+@implementation TRRoutingTable
+
+@end
