a35061ee19ec7b7afaec5b3c8ee2a6a7c8a36acc
Kota Mizushima <mizukota@gmail.com>
2012-06-21 14:31:58 +0900

    Add document to TRCinatraRouter

--- Classes/Routing/TRCinatraRouter.h
+++ Classes/Routing/TRCinatraRouter.h
@@ -10,10 +10,28 @@
 #import "TRAction.h"
 #import "TRBlocksAction.h"
 
+/**
+ * |TRCinatraRouter| is a subclass of |TRRequestRouter|.
+ * It supports Sinatra-like notation in addition to normal notation.
+ */
 @interface TRCinatraRouter : TRRequestRouter
 
+/**
+ * |[self get:pathPattern on:action]| is just shorthand of
+ * |[self addRouteForGET:pathPattern to:[BlocksAction newAction:action]]|.
+ */
 - (void)get:(NSString*)pathPattern on:(ActionFunction)action;
+
+/**
+ * |[self post:pathPattern on:action]| is just shorthand of
+ * |[self addRouteForPOST:pathPattern to:[BlocksAction newAction:action]]|.
+ */
 - (void)post:(NSString*)pathPattern on:(ActionFunction)action;
+
+/**
+ * |[self put:pathPattern on:action]| is just shorthand of
+ * |[self addRouteForPUT:pathPattern to:[BlocksAction newAction:action]]|.
+ */
 - (void)put:(NSString*)pathPattern on:(ActionFunction)action;
 
 @end
