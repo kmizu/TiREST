62d2e475960344618c831f313d754ae302a338b0
Kota Mizushima <mizukota@gmail.com>
2012-06-21 16:23:41 +0900

    Prepare for unit test

---
+++ Tests/Unit/TiRESTUnitTestBase.h
@@ -0,0 +1,13 @@
+//
+//  TiRESTUnitTestBase.h
+//  TiREST
+//
+//  Created by Kota Mizushima on 12/06/21.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TiRESTUnitTestBase : SenTestCase
+
+@end
---
+++ Tests/Unit/TiRESTUnitTestBase.m
@@ -0,0 +1,13 @@
+//
+//  TiRESTUnitTestBase.m
+//  TiREST
+//
+//  Created by Kota Mizushima on 12/06/21.
+//
+//
+
+#import "TiRESTUnitTestBase.h"
+
+@implementation TiRESTUnitTestBase
+
+@end
